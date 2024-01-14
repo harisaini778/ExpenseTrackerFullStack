2f5b3afad519563f39747541dc82d9daa1ae7cbf
harisaini778 <harikumarsaini778@gmail.com>
2024-01-14 17:08:29 +0530

    added the code in server.js file

--- Backend/server.js
+++ Backend/server.js
@@ -1,2 +1,27 @@
+/* /backend/server.js */
 
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const createConnection = require('./models/database');
+const UserController = require('./controller/userController');
 
+const app = express();
+
+// Middleware to parse JSON in the request body
+app.use(bodyParser.json());
+
+// Enable CORS
+app.use(cors());
+
+// Establish database connection
+createConnection(); // Assuming this function sets up your database connection
+
+// User Controllers access
+app.use('/users', UserController);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}.`);
+});
